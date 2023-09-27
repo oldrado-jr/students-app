595762b4c4bc88ede708b615705be1017e0dac66
Oldrado Junior <oldradojunior@gmail.com>
2023-09-27 02:22:22 -0300

    feat: add student deletion

--- src/pages/Students/index.jsx
+++ src/pages/Students/index.jsx
@@ -1,7 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { toast } from 'react-toastify';
 import { get } from 'lodash';
-import { FaEdit, FaUserCircle, FaWindowClose } from 'react-icons/fa';
+import {
+  FaEdit,
+  FaUserCircle,
+  FaWindowClose,
+  FaExclamation,
+} from 'react-icons/fa';
 
 import { Container } from '../../styles/GlobalStyles';
 import axios from '../../services/axios';
@@ -10,6 +15,33 @@ import { primaryColor } from '../../config/colors';
 
 export default function Students() {
   const [students, setStudents] = useState([]);
+
+  const handleDeleteConfirmation = (e) => {
+    e.preventDefault();
+    const deleteIcon = e.currentTarget;
+    const exclamationIcon = deleteIcon.nextSibling;
+    exclamationIcon.setAttribute('display', 'inline');
+    deleteIcon.remove();
+  };
+
+  const handleDelete = async (e, id, index) => {
+    e.preventDefault();
+
+    try {
+      await axios.delete(`/alunos/${id}`);
+      const newStudents = [...students];
+      newStudents.splice(index, 1);
+      setStudents(newStudents);
+    } catch (error) {
+      const status = get(error, 'response.status', 0);
+
+      if (status === 401) {
+        toast.error('Você precisa fazer login!');
+      } else {
+        toast.error('Houve um erro ao excluir aluno!');
+      }
+    }
+  };
 
   useEffect(() => {
     (async () => {
@@ -26,7 +58,7 @@ export default function Students() {
     <Container>
       <h1>Alunos</h1>
       <StudentContainer>
-        {students.map((student) => (
+        {students.map((student, index) => (
           <li key={String(student.id)}>
             <ProfilePhoto>
               {get(student, 'Fotos[0].url', false) ? (
@@ -39,8 +71,20 @@ export default function Students() {
             <span>{student.nome}</span>
             <span>{student.email}</span>
 
-            <FaEdit size={16} color={primaryColor} />
-            <FaWindowClose size={16} color={primaryColor} />
+            <FaEdit size={16} color={primaryColor} cursor="pointer" />
+            <FaWindowClose
+              onClick={handleDeleteConfirmation}
+              size={16}
+              color={primaryColor}
+              cursor="pointer"
+            />
+            <FaExclamation
+              onClick={(e) => handleDelete(e, student.id, index)}
+              size={16}
+              color={primaryColor}
+              display="none"
+              cursor="pointer"
+            />
           </li>
         ))}
       </StudentContainer>
